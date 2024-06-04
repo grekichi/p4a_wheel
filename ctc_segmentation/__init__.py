aa125c6c4f03281670858096cc333953561b10ef
grekichi <87578278+grekichi@users.noreply.github.com>
2024-06-04 14:51:46 +0900

    Delete ctc_segmentation directory

--- ctc_segmentation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Import all functions of the CTC segmentation package."""
-from .ctc_segmentation import ctc_segmentation
-from .ctc_segmentation import CtcSegmentationParameters
-from .ctc_segmentation import determine_utterance_segments
-from .ctc_segmentation import prepare_text
-from .ctc_segmentation import prepare_tokenized_text
-from .ctc_segmentation import prepare_token_list
-from .partitioning import get_partitions
